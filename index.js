0d0075f8b9e461a58a79a08a4cf76f01be45cf72
Carina Cruz <lecruz@bu.edu>
2018-10-04 23:09:11 +0000

    scheduling tasks

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
     // index.js
     /*global app*/ 
-    const cron = require("node-cron");
-    const express = require("express");
-    const fs = require("fs");
+    const cron = require("node-cron");  //task scheduler in JS for node.js
+    const express = require("express"); //powers the web server
+    const fs = require("fs"); //node file systme module 
 
     app = express();
 
-// schedule tasks to be run on the server   
-    cron.schedule("* * * * *", function() {
-      console.log("running a task every minute");
+//deleting the log file from the server on the 1st of every month 
+  
+      cron.schedule(" * * 21 * *", function() {
+      console.log("---------------------");
+      console.log("Running Cron Job");
+      fs.unlink("./error.log", err => {
+        if (err) throw err;
+        console.log("Error file succesfully deleted");
+      });
     });
 
-    app.listen(3128);
+    app.listen("3128");
+    
+
+
