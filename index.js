29d719537d9efdd600a3e44dad2ddd1601ba6daa
Carina Cruz <lecruz@bu.edu>
2018-10-04 22:44:27 +0000

    adding the backend server

---
+++ index.js
@@ -0,0 +1,14 @@
+    // index.js
+    /*global app*/ 
+    const cron = require("node-cron");
+    const express = require("express");
+    const fs = require("fs");
+
+    app = express();
+
+// schedule tasks to be run on the server   
+    cron.schedule("* * * * *", function() {
+      console.log("running a task every minute");
+    });
+
+    app.listen(3128);
