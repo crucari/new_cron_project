8a0cf8d9fdafdaf231d5fa05cfea2946bafed478
Carina Cruz <lecruz@bu.edu>
2018-10-10 23:59:22 +0000

    database update

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
+//     // index.js
+//     /*global app*/ 
+//     const cron = require("node-cron");
+//     const express = require("express");
+//     const fs = require("fs");
+
+//     app = express();
+
+// // schedule tasks to be run on the server   
+//     cron.schedule("* * * * *", function() {
+//       console.log("running a task every minute");
+//     });
+
+//     app.listen(3128);
+    
     // index.js
-    /*global app*/ 
-    const cron = require("node-cron");
-    const express = require("express");
     const fs = require("fs");
+    let shell = require("shelljs");
+    const express = require("express");
 
     app = express();
 
-// schedule tasks to be run on the server   
-    cron.schedule("* * * * *", function() {
-      console.log("running a task every minute");
+    // To backup a database
+    cron.schedule("59 23 * * *", function() {
+      console.log("---------------------");
+      console.log("Running Cron Job");
+      if (shell.exec("sqlite3 database.sqlite  .dump > data_dump.sql").code !== 0) {
+        shell.exit(1);
+      }
+      else{
+        shell.echo("Database backup complete");
+      }
     });
-
-    app.listen(3128);
+    app.listen("3128");
